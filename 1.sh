932d5dd956ab877cd7aae68e121cc9976a37ac12
Koshelev Daniil <debl1n@yandex.ru>
2020-09-10 18:58:35 +0400

    Remove garbage lines

--- 1.sh
+++ 1.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-#export FILENAME_PATTERN=$1
-#export CONTENT_PATTERN=$2
 if [ -n "$1" ] && [ -n "$2" ]
-then 
+then
 export FILENAME_PATTERN=$1
 export CONTENT_PATTERN=$2
-echo "Success"
-else 
+else
 export FILENAME_PATTERN=test?.*
 export CONTENT_PATTERN=?62*
-echo "Not success"
 fi
 content_p=${CONTENT_PATTERN//\?/.}
 content_p=${content_p//\*/.*}
