8839c74b808b866be696bbd83335fb36612aded5
Koshelev Daniil <debl1n@yandex.ru>
2020-09-11 12:43:34 +0400

    Backup script

--- 2.sh
+++ 2.sh
@@ -1,4 +1,8 @@
-if [ -n "$1" ]
-then echo "First argumetn is ${1}"
-else echo "Nothing"
-fi
+#!/bin/bash
+d=$(date '+%Y-%m-%d-%H-%M-%S')
+source ~/settings.conf
+path=/tmp/backup-$(whoami)-$d.tgz
+tar -cvf $path $dir
+# f - to update link
+ln -fs $path $link_name
+
